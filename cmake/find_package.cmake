12a3905e6f75fd3271b7eca1637e433d32a5ab90
Florian Becker <fb@vxapps.com>
2021-11-24 17:29:50 +0100

    Fix gtkware target for mac os

--- cmake/find_package.cmake
+++ cmake/find_package.cmake
@@ -29,6 +29,6 @@
 #
 
 find_program(GTKWAVE NAMES gtkwave)
-if(APPLE AND EXISTS "/Applications/gtkwave.app")
-  set(GTKWAVE "open /Applications/gtkwave.app")
+if(APPLE AND EXISTS /Applications/gtkwave.app)
+  set(GTKWAVE open /Applications/gtkwave.app)
 endif()
